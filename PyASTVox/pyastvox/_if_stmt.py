333fc2bc4fa08e34cc82f76a7c75cc82fd5941b3
sc4311 <84731471+sc4311@users.noreply.github.com>
2024-01-10 22:45:03 -0600

    Implemented if-stmt
    If you could advise over if I completed the statement how you wanted that would be great. However the examples are the same as what the code is written.

--- PyASTVox/pyastvox/_if_stmt.py
+++ PyASTVox/pyastvox/_if_stmt.py
@@ -7,20 +7,44 @@ import ast
 
 class if_stmt_mixin:
     def gen_ast_If_default(self, node):
+        print(node.test.jvox_speech)
         '''
         Generate speech for ast.IfExp. Style "default"
+        
+        wrote in call: 
+        if a>b return =>
+        
+        if (a > b) {
+            return '=>'
+        }
+        
+        
+        Examples:
+        1. e.g.,a > b if a > b else b: a greater than b if a greater than b else b
+        
+        2. e.g.,a > b if a > b else b if a > b else c: a greater than b if a greater than b else b if a greater than b else c
         '''
 
         style_name = "default"
 
-        print(node.test.jvox_speech)
+        # generate the speech for the test
+        test_speech = node.test.jvox_speech["default"] + " "
+
+        # generate the speech for the body
+        body_speech = "if " + node.body.jvox_speech["default"] + " "
+
+        # generate the speech for the orelse
+        orelse_speech = "else " + node.orelse.jvox_speech["default"]
+
+        # generate the final speech
+        speech = test_speech + body_speech + orelse_speech
 
         # add the speech to jvox_speech
         if not hasattr(node, "jvox_speech"):
             node.jvox_speech = {}
-            node.jvox_speech[style_name] = "To be generated"
+        node.jvox_speech[style_name] = speech
 
-        return
+        return speech
 
     def gen_ast_If(self, node):
         '''
